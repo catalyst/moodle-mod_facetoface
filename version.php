92e28fdc3c1042980f2c3c79acc034ea2cc5ad7a
Stacey Walker <stacey@catalyst-eu.net>
2014-02-27 17:56:12 +0000

    version.php: bumping to version 2.2.4.1

--- version.php
+++ version.php
@@ -5,9 +5,9 @@
 //  This fragment is called by /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$module->version   = 2013010400;  // use minor version bumps until 2013 then use YYYYMMDDxx
-$module->requires  = 2011120500;  // Requires this Moodle version
-$module->release   = '2.2.3.1 (2013010400)'; // User-friendly version number
+$module->version   = 2014022700;  // use minor version bumps until 2013 then use YYYYMMDDxx
+$module->requires  = 2012120308;  // Requires this Moodle version
+$module->release   = '2.2.4.1 (Build: 2014022700)'; // User-friendly version number
 $module->component = 'mod_facetoface';
 $module->maturity  = MATURITY_STABLE;
 $module->cron      = 60;
