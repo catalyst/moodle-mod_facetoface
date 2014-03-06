0f2bda42fe4c6bd2343f7e54f0415fa7ac6ccf76
Stacey Walker <stacey@catalyst-eu.net>
2014-03-06 15:17:45 +0000

    Bump to fix ChangeLog

--- version.php
+++ version.php
@@ -5,9 +5,9 @@
 //  This fragment is called by /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$module->version   = 2014022700;  // use minor version bumps until 2013 then use YYYYMMDDxx
+$module->version   = 2014030600;  // use minor version bumps until 2013 then use YYYYMMDDxx
 $module->requires  = 2012120308;  // Requires this Moodle version
-$module->release   = '2.2.4.1 (Build: 2014022700)'; // User-friendly version number
+$module->release   = '2.2.4.2 (Build: 2014030600)'; // User-friendly version number
 $module->component = 'mod_facetoface';
 $module->maturity  = MATURITY_STABLE;
 $module->cron      = 60;
