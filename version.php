7374a2fc33ca786986a40ddee693621cd4ebc682
Stacey Walker <stacey@catalyst-eu.net>
2014-03-06 15:50:28 +0000

    version.php: bump to development versioning

--- version.php
+++ version.php
@@ -7,7 +7,7 @@
 
 $module->version   = 2014030600;  // use minor version bumps until 2013 then use YYYYMMDDxx
 $module->requires  = 2012120308;  // Requires this Moodle version
-$module->release   = '2.2.4.2 (Build: 2014030600)'; // User-friendly version number
+$module->release   = '2.2.4.2+ (Build: 2014030600)'; // User-friendly version number
 $module->component = 'mod_facetoface';
 $module->maturity  = MATURITY_STABLE;
 $module->cron      = 60;
