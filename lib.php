b579cab5bc49713af585f7bf1339bc62138ffd9c
Stacey Walker <stacey@catalyst-eu.net>
2015-12-04 12:56:26 +0100

    lib: remove unnecessary check for core library file

--- lib.php
+++ lib.php
@@ -34,9 +34,7 @@ require_once($CFG->libdir . '/gradelib.php');
 require_once($CFG->dirroot . '/grade/lib.php');
 require_once($CFG->dirroot . '/lib/adminlib.php');
 require_once($CFG->dirroot . '/user/selector/lib.php');
-if (file_exists($CFG->libdir . '/completionlib.php')) {
-    require_once($CFG->libdir . '/completionlib.php');
-}
+require_once($CFG->libdir . '/completionlib.php');
 
 /*
  * Definitions for setting notification types.
