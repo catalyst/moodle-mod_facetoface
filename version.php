e98ca29a146864e79b115c89358aecf3cb552cb2
Dan Marsden <dan@danmarsden.com>
2018-07-05 11:33:27 +1200

    Bump version for plugins db release.

--- version.php
+++ version.php
@@ -30,8 +30,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018062000;
+$plugin->version   = 2018070500;
 $plugin->requires  = 2017111300;  // Requires 3.4.
-$plugin->release   = '3.4.1';
+$plugin->release   = '3.4.2';
 $plugin->component = 'mod_facetoface';
 $plugin->maturity  = MATURITY_STABLE;
