1d5468437046394b0c7695c9c9ac0d144fea3c1e
System Administrator <root@Hamishs-MacBook-Pro-2.local>
2019-05-10 08:00:17 +1200

    Allows use of new TLDs and '+' in the from address

--- settings.php
+++ settings.php
@@ -37,7 +37,7 @@ $settings->add(new admin_setting_configtext(
     get_string('setting:fromaddress_caption', 'facetoface'),
     get_string('setting:fromaddress', 'facetoface'),
     get_string('setting:fromaddressdefault', 'facetoface'),
-    "/^((?:[\w\.\-])+\@(?:(?:[a-zA-Z\d\-])+\.)+(?:[a-zA-Z\d]{2,4}))$/",
+    "/^((?:[\w\.\-\+])+\@(?:(?:[a-zA-Z\d\-])+\.)+(?:[a-zA-Z\d]{2,}))$/",
     30
 ));
 
