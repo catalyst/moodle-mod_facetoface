ff2a11b479fe49b7a671942bdbe314c234cce648
Dan Marsden <dan@danmarsden.com>
2020-08-04 11:37:26 +1200

    bump version for 3.9 support.

--- version.php
+++ version.php
@@ -30,8 +30,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018110900;
+$plugin->version   = 2020080400;
 $plugin->requires  = 2017111300;  // Requires 3.4.
-$plugin->release   = '3.4.3';
+$plugin->release   = '3.9.1';
 $plugin->component = 'mod_facetoface';
 $plugin->maturity  = MATURITY_STABLE;
