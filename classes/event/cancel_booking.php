7f399519c5696ed7017d35a9e708b38aad5c7e25
Matthias Opitz <m.opitz@ucl.ac.uk>
2024-03-01 15:32:41 +0000

    removed deprecated legacy methods

--- classes/event/cancel_booking.php
+++ classes/event/cancel_booking.php
@@ -80,16 +80,6 @@ class cancel_booking extends \core\event\base {
     }
 
     /**
-     * Return the legacy event log data.
-     *
-     * @return array|null
-     */
-    protected function get_legacy_logdata() {
-        return array($this->courseid, $this->objecttable, 'cancel booking', 'cancelsignup.php?s=' . $this->objectid,
-            $this->objectid, $this->contextinstanceid);
-    }
-
-    /**
      * Custom validation.
      *
      * @throws \coding_exception
--- classes/event/update_session_failed.php
+++ classes/event/update_session_failed.php
@@ -80,16 +80,6 @@ class update_session_failed extends \core\event\base {
     }
 
     /**
-     * Return the legacy event log data.
-     *
-     * @return array|null
-     */
-    protected function get_legacy_logdata() {
-        return array($this->courseid, $this->objecttable, 'update session (FAILED)', 'sessions.php?s=' . $this->objectid,
-            $this->objectid, $this->contextinstanceid);
-    }
-
-    /**
      * Custom validation.
      *
      * @throws \coding_exception
