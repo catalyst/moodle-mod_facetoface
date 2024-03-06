a09c4504b426fd5d80866acb3e8754a9b979248a
Dan Marsden <dan@danmarsden.com>
2024-03-07 12:05:24 +1300

    bump version for release.

--- version.php
+++ version.php
@@ -30,8 +30,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024030100;
-$plugin->release   = 2024030100;
+$plugin->version   = 2024030700;
+$plugin->release   = 2024030700;
 $plugin->requires  = 2023100900;  // Requires 4.3
 $plugin->component = 'mod_facetoface';
 $plugin->maturity  = MATURITY_STABLE;
