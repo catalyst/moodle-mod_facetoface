48ee56e4574a185263011ebcb4007d12fd28e933
djarrancotleanu <djarrancotleanu@catalyst-au.net>
2025-06-17 13:08:16 +1000

    Issue #199: Add new visibleto column in facetoface_session_field table

--- db/upgrade.php
+++ db/upgrade.php
@@ -874,5 +874,37 @@ function xmldb_facetoface_upgrade($oldversion=0) {
         upgrade_mod_savepoint(true, 2025052303, 'facetoface');
     }
 
+    if ($oldversion < 2025052304) { // Replace with your version number
+
+        // Define field visibleto to be added to facetoface_session_field
+        $table = new xmldb_table('facetoface_session_field');
+        $field = new xmldb_field(
+            'visibleto',
+            XMLDB_TYPE_INTEGER,
+            '1',
+            null,
+            XMLDB_NOTNULL,
+            null,
+            MDL_F2F_FIELD_VISIBLETOALL,
+            'showinsummary'
+        );
+
+        // Conditionally launch add field visibleto.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+
+            // Get all existing records and set visibleto based on showinsummary value.
+            $recordset = $DB->get_recordset('facetoface_session_field');
+            foreach ($recordset as $record) {
+                $record->visibleto = empty($record->showinsummary) ? MDL_F2F_FIELD_NOTVISIBLE : MDL_F2F_FIELD_VISIBLETOALL;
+                $DB->update_record('facetoface_session_field', $record);
+            }
+            $recordset->close;
+        }
+
+        // Face-to-face savepoint reached
+        upgrade_mod_savepoint(true, 2025052304, 'facetoface');
+    }
+
     return $result;
 }
